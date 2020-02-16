4e3baf05bf38e59ab540451944b57fd4fe980859
Rishabh Sharma <risharma654@gmail.com>
2020-02-16 23:39:25 +0530

    defaultTemplate

---
+++ DefaultTemplate.cpp
@@ -0,0 +1,13 @@
+// Default template:
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+//will update soon
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
